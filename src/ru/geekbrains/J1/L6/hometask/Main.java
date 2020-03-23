bceece8a7a1f08aa6c787f08f9610c5ee67aed68
teriberka <box4trash@inbox.ru>
2020-03-23 19:10:04 +0700

    add hometask 6

--- src/ru/geekbrains/J1/L6/hometask/Main.java
+++ src/ru/geekbrains/J1/L6/hometask/Main.java
@@ -1,12 +1,12 @@
 package ru.geekbrains.J1.L6.hometask;
 
-import java.io.FileInputStream;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 public class Main {
 
